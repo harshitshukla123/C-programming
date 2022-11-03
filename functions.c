856d9838950e61e370e8b1f154b9124fb30c946c
harshitshukla123 <104128682+harshitshukla123@users.noreply.github.com>
2022-11-03 18:15:05 +0530

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void display();
+int main(){
+    int a;
+    printf("Initialising display\n");
+    display();
+    printf("the work is over of display\n");
+    return 0;
+}
+void display(){
+    printf("This is display\n");
+}
