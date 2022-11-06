33380491582242a80aa9dc0b8c2d0098425d2502
harshitshukla123 <104128682+harshitshukla123@users.noreply.github.com>
2022-11-06 23:18:39 +0530

    Add files via upload

---
+++ arraysmarksof5stu.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    //take the inpt the marks of five students and print them on the screen
+    int arr[5]={67,55,34,85,35};
+    for(a=0;a<5;a++){
+    printf("%d\t",arr[a]);
+    }
+    return 0;
+}
